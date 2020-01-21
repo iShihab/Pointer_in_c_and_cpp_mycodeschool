5b9946031e93ed3c948f2040a1287e95f17d5f19
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-01-22 00:55:23 +0600

    Update Pointers_to_Pointers_in_CCPP.c

--- Pointers_to_Pointers_in_CCPP.c
+++ Pointers_to_Pointers_in_CCPP.c
@@ -8,7 +8,7 @@ int main(int argc, char const *argv[])
     int **q = &p;
     **q = 7; //p is already a pointer and q itself a pointer so **q
     int ***r = &q;
-    ***r = 8;                 // r->q->p->p->x q is a pointer and its pointing p which also a pointer and r itself a pointer so ***r
+    ***r = 8;                 //this will change the value of x to 8              // r->q->p->p->x q is a pointer and its pointing p which also a pointer and r itself a pointer so ***r
     printf("%d\n", *p);       //p->x    output=6 as *p=6
     printf("%d\n", *q);       //q->p   output will be the address of p
     printf("%d\n", *(*q));    //q->p->x output will be the value of x
