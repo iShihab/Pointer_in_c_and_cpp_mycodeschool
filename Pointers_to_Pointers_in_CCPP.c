13b9aa643a5662e8fae3731ae007a04dcdf3fa7b
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-01-22 00:53:38 +0600

    Changing the value of x

--- Pointers_to_Pointers_in_CCPP.c
+++ Pointers_to_Pointers_in_CCPP.c
@@ -4,9 +4,11 @@ int main(int argc, char const *argv[])
 {
     int x = 5;
     int *p = &x;
-    *p = 6;                   //p storing an address of value 6,thats why its a single star
-    int **q = &p;             //p is already a pointer and q itself a pointer so **q
-    int ***r = &q;            // q is a pointer and its pointing p which also a pointer and r itself a pointer so ***r
+    *p = 6; //p storing an address of value 6,thats why its a single star
+    int **q = &p;
+    **q = 7; //p is already a pointer and q itself a pointer so **q
+    int ***r = &q;
+    ***r = 8;                 // r->q->p->p->x q is a pointer and its pointing p which also a pointer and r itself a pointer so ***r
     printf("%d\n", *p);       //p->x    output=6 as *p=6
     printf("%d\n", *q);       //q->p   output will be the address of p
     printf("%d\n", *(*q));    //q->p->x output will be the value of x
