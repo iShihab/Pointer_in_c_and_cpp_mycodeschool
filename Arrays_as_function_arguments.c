cede0dd9948729eb440daef8bfad8599908aeb80
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-01-22 03:09:54 +0600

    finding out size of a array in c

---
+++ Arrays_as_function_arguments.c
@@ -0,0 +1,24 @@
+// Arrays as function argument
+#include <stdio.h>
+
+void printSum(int x[], int s)
+{
+    int i, sum = 0;
+    for (i = 0; i < s; i++)
+    {
+        sum = sum + i;
+    }
+    printf("Sum = %d", sum);
+}
+
+int main(int argc, char const *argv[])
+{
+    int a[] = {2, 4, 6, 8, 9};
+    int size = sizeof(a) / sizeof(a[3]); //sizeof(a) return whole size of array a which is 20
+    //to get the total element in the array a
+    // size =sizeof(a)20/sizeofanyelement of the array(a[3])4=5
+    //size =20/4=5
+
+    printSum(a, size);
+    return 0;
+}
