38b21226b97019a251ef167840192687a98fe15f
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-01-22 03:59:49 +0600

    In Arrays there is no call be value there is always call by reference

--- Arrays_as_function_arguments.c
+++ Arrays_as_function_arguments.c
@@ -2,20 +2,24 @@
 // In Arrays there is no call be value there is always call by reference
 #include <stdio.h>
 
-void printSum(int x[])
+void Double(int *A, int size)
 {
-    int i, sum = 0;
-    int s = sizeof(x) / sizeof(x[0]);
-    for (i = 0; i < s; i++)
+    int i;
+    for (i = 0; i < size; i++)
     {
-        sum = sum + x[i];
+        A[i] = 2 * A[i];
     }
-    printf("Sum = %d", sum);
 }
-
 int main(int argc, char const *argv[])
 {
     int a[] = {2, 4, 6, 8, 9};
-    printSum(a);
+    int size = sizeof(a) / sizeof(a[0]);
+    Double(a, size);
+    int i;
+    for (i = 0; i < size; i++)
+    {
+        printf("%d ", a[i]);
+    }
+
     return 0;
 }
