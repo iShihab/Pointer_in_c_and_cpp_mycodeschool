322f3677d3d4f7d68da8640ad34c408bd8b1f88a
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-01-22 03:13:57 +0600

    problem fixed on printSum

--- Arrays_as_function_arguments.c
+++ Arrays_as_function_arguments.c
@@ -6,7 +6,7 @@ void printSum(int x[], int s)
     int i, sum = 0;
     for (i = 0; i < s; i++)
     {
-        sum = sum + i;
+        sum = sum + x[i];
     }
     printf("Sum = %d", sum);
 }
@@ -14,7 +14,7 @@ void printSum(int x[], int s)
 int main(int argc, char const *argv[])
 {
     int a[] = {2, 4, 6, 8, 9};
-    int size = sizeof(a) / sizeof(a[3]); //sizeof(a) return whole size of array a which is 20
+    int size = sizeof(a) / sizeof(a[0]); //sizeof(a) return whole size of array a which is 20
     //to get the total element in the array a
     // size =sizeof(a)20/sizeofanyelement of the array(a[3])4=5
     //size =20/4=5
