39115d05355e1be6d5e896388dfc56949293b8f2
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-01-22 00:38:51 +0600

    pointer

--- Pointers_to_Pointers_in_CCPP.c
+++ Pointers_to_Pointers_in_CCPP.c
@@ -4,12 +4,14 @@ int main(int argc, char const *argv[])
 {
     int x = 5;
     int *p = &x;
-    *p = 6;
+    *p = 6; //p storing an address of value 6,thats why its a single star
     int **q = &p;
     int ***r = &q;
     printf("%d\n", *p);
     printf("%d\n", *q);
     printf("%d\n", *(*q));
+    printf("%d\n", **r);
+    printf("%d\n", *(*(*r)));
 
     return 0;
 }
