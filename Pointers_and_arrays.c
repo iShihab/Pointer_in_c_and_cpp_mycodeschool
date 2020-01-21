02da7e3320873ae90a54b0b7b979c7e3f8cc6dee
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-01-22 02:53:18 +0600

    Different way to get the value and address of a variable

--- Pointers_and_arrays.c
+++ Pointers_and_arrays.c
@@ -1,2 +1,14 @@
 // For address--- &A[i] or (A+i)
 // For value--- A[i] or *(A+i)
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int A[] = {2, 4, 5, 8, 1};
+    printf("%d\n", A);
+    printf("%d\n", &A[0]);
+    printf("%d\n", A[0]);
+    printf("%d\n", *A);
+    return 0;
+}
