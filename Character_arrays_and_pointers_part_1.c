abdf46c9bdfb8bbf6f4be868abf832ccc28b500f
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-01-22 23:37:06 +0600

    this will also work

--- Character_arrays_and_pointers_part_1.c
+++ Character_arrays_and_pointers_part_1.c
@@ -4,10 +4,10 @@
 void print(char *c)
 {
     int i = 0;
-    while (*(c + i) != '\0')
+    while (*c != '\0')
     {
-        printf("%c", c[i]);
-        i++;
+        printf("%c", *c);
+        c++;
     }
     printf("\n");
 }
