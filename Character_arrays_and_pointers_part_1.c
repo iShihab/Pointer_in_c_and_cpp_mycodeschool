4ad9ace9ff68adf19b59c99f279ee3e1c65f317c
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-01-22 23:35:06 +0600

    using pointer to print character

--- Character_arrays_and_pointers_part_1.c
+++ Character_arrays_and_pointers_part_1.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 #include <string.h>
+
+void print(char *c)
+{
+    int i = 0;
+    while (c[i] != '\0')
+    {
+        printf("%c", c[i]);
+        i++;
+    }
+    printf("\n");
+}
+
 int main(int argc, char const *argv[])
 {
-    char c[5] = "SAJED";
-    int len = strlen(c);
-    printf("Size in bytes = %d\n", sizeof(c));
-    printf("Length = %d\n", len);
-    return 0;
+    char c[20] = "Hello";
+    printf(c);
 }
