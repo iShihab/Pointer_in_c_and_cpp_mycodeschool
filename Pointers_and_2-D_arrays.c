d82eb2540f691dd41b6f1c79dd170daa5940b3ec
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-01-23 23:19:02 +0600

    creating 2 one dimensional array with 3 element in each

--- Pointers_and_2-D_arrays.c
+++ Pointers_and_2-D_arrays.c
@@ -1,23 +1,7 @@
 #include <stdio.h>
 
-// void printArray(int *a, int size)
-// {
-//     int i = 0;
-//     for (i = 0; i < size; i++)
-//     {
-//         printf("%d\n", *(a + i));
-//     }
-// }
-
 int main(int argc, char const *argv[])
 {
-    int a[5] = {2, 4, 6, 8, 10};
-    int *p = a;
-    int i = 0;
-    for (i = 0; i < 5; i++)
-    {
-        printf("%d\n", *(p + i));
-    }
-
+    int b[2][3]; //creating 2 one dimensional array with 3 element in each
     return 0;
 }
