990a7f713e9361a5a6b6415a528d014d1d0ee26e
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-01-23 06:21:41 +0600

    Difference between *a,*a + 1,*(a + 1)

--- Pointers_and_2-D_arrays.c
+++ Pointers_and_2-D_arrays.c
@@ -8,7 +8,9 @@ int main(int argc, char const *argv[])
 {
     int a[2][3] = {1, 2, 3, 4, 5, 6};
     // printArray(a);
-    printf("%d", *(a + 1));
+    printf("%d\n", *a);
+    printf("%d", *a + 1);
+    printf("\n%d", *(a + 1));
 
     return 0;
 }
