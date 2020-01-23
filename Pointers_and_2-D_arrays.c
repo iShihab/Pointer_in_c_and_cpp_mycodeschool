06990c9731bbe7d8d1ac5bca91f867a761b83ab3
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-01-24 02:54:50 +0600

    Update Pointers_and_2-D_arrays.c

--- Pointers_and_2-D_arrays.c
+++ Pointers_and_2-D_arrays.c
@@ -2,6 +2,7 @@
 
 int main(int argc, char const *argv[])
 {
-    int b[2][3]; //creating 2 one dimensional array with 3 element in each
+    int b[2][3] = {2, 3, 6, 4, 5, 8}; //creating 2 one dimensional array with 3 element in each
+    printf("%d", *(b + 1) + 2);
     return 0;
 }
