5d9df361dc8c2b39728e49ab3d8150c702026cda
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-01-23 06:18:48 +0600

    Update Pointers_and_2-D_arrays.c

--- Pointers_and_2-D_arrays.c
+++ Pointers_and_2-D_arrays.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
 
+void printArray(int *b)
+{
+}
+
 int main(int argc, char const *argv[])
 {
-    int i = 0, j = 0;
     int a[2][3] = {1, 2, 3, 4, 5, 6};
-    for (i = 0; i < 2; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            printf("%d ", a[i][j]);
-        }
-        printf("\n");
-    }
+    // printArray(a);
+    printf("%d", *a + 1);
 
     return 0;
 }
