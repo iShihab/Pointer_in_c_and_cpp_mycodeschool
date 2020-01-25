4552f86b5ca82a299eeaf23ce09d82bb4ceea22b
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-01-25 20:27:02 +0600

    Update 2_D_Array.c

--- 2_D_Array.c
+++ 2_D_Array.c
@@ -4,6 +4,7 @@ int main(int argc, char const *argv[])
 {
     int twoDArray[2][3] = {1, 2, 3, 4, 5, 6}; //two one dimensional array with three elements each
     int(*p)[3] = twoDArray;
+    printf("Output of p[0]: %d\n ", *p[0]);
 
     return 0;
 }
