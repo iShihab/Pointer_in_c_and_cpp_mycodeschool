5f7601076b9d422a4c9eae87b90b16203d3c7b17
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-01-25 21:43:32 +0600

    Update 2_D_Array.c

--- 2_D_Array.c
+++ 2_D_Array.c
@@ -3,7 +3,7 @@
 int main(int argc, char const *argv[])
 {
     int b[2][3] = {2, 3, 6, 4, 5, 8};
-    printf("%d\n", b);
-    printf("%d\n", b[0]);
+    printf("b or &b[0] = %d or %d\n", b, &b[0]);
+    printf("*b or b[0] or  &b[0][0] = %d or %d or %d", *b, b[0], &b[0][0]); //Return first value's address of 1D array
     return 0;
 }
