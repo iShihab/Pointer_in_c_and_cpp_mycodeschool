1b2244242e16f282349b966745e05e7a43fbc7cb
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-01-25 20:20:27 +0600

    Compilation error

--- 2_D_Array.c
+++ 2_D_Array.c
@@ -3,5 +3,7 @@
 int main(int argc, char const *argv[])
 {
     int twoDArray[2][3] = {1, 2, 3, 4, 5, 6}; //two one dimensional array with three elements each
+    int *p = twoDArray;
+
     return 0;
 }
