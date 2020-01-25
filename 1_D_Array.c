e2a2dc5ca218cd5151de08e3ccecbca6d7029770
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-01-25 20:05:17 +0600

    Create 1_D_Array.c

---
+++ 1_D_Array.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int OneDArray[] = {2, 4, 6, 8, 10};
+    int *p = OneDArray;
+    printf("Output for p: %d\n", p);
+    printf("Output for *p: %d\n", *p);
+    printf("Output for p+1: %d\n", p + 1);
+    return 0;
+}
