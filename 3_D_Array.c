68dc623d2777b721e005b29845eccb5de3ccedb7
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-01-26 09:22:09 +0600

    Create 3_D_Array.c

---
+++ 3_D_Array.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int c[3][2][2] = {{{2, 5}, {7, 9}},
+                      {{3, 4}, {6, 1}},
+                      {{0, 8}, {11, 13}}};
+
+    printf("%d %d %d %d\n", c, *c, c[0], &c[0][0]);
+    printf("%d\n", **(*c + 0 + 2));
+
+    return 0;
+}
