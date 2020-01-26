2bf3dfeab16dde85b4e6210422488bad55d59a70
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-01-26 18:58:44 +0600

    Update test.c

--- test.c
+++ test.c
@@ -1,28 +1,27 @@
 #include <stdio.h>
-
-int main(int argc, char const *argv[])
+int main()
 {
-    int b[2][3] = {2, 3, 6, 4, 5, 8};
-    // printf("b or &b[0] = %d or %d\n", b, &b[0]);
-    // printf("*b or b[0] or  &b[0][0] = %d or %d or %d", *b, b[0], &b[0][0]); //Return first value's address of 1D array
-    // printf("%d\n", &b[0]);
-    // printf("%d\n", b[0]);
-    // printf("%d\n", b);
-    // printf("%d\n", *b);
-    // printf("%d\n", b);
-    // printf("%d\n", b[2]);
-    // printf("%d\n", **b);
-    // printf("%d\n", *b[0]);
-    // printf("%d\n", *&b[0][0]);
-    // printf("%d\n", **(b + 0));
-    // printf("%d\n", *(*(b + 1) + 2));
-    // printf("%d\n", *b);
-    // printf("%d\n", *(b + 0));
-    printf("%d\n", *(*b + 1));
-
-    // printf("%d\n", (b + 1));
-    // printf("%d\n", **(b + 1));
-    // printf("%d\n", *(b[0] + 1));
+    int arr[2][3][2] = {
+        {
+            {5, 10},
+            {6, 11},
+            {7, 12},
+        },
+        {
+            {20, 30},
+            {21, 31},
+            {22, 32},
+        }};
+    int i, j, k;
+    for (i = 0; i < 2; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            for (k = 0; k < 2; k++)
+                printf("%d\t", *(*(*(arr + i) + j) + k));
+            printf("\n");
+        }
+    }
 
     return 0;
 }
