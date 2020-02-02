1f9b2d4c79442ccf14029361f2b116d9263fdeab
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-02-03 05:53:14 +0600

    Update tempCodeRunnerFile.c

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,1 +1,10 @@
- // printf("%d\n", );
+ // int i, j, k;
+    // for (i = 0; i < 2; i++)
+    // {
+    //     for (j = 0; j < 3; j++)
+    //     {
+    //         for (k = 0; k < 2; k++)
+    //             printf("%d\t", *(*(*(arr + i) + j) + k));
+    //         printf("\n");
+    //     }
+    // }
