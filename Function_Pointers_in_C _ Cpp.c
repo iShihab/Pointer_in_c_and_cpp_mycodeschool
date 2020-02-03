1a23a07ffcdceb06d5e7f8ead60521f0be3ffe02
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-02-03 06:40:52 +0600

    pointer that point function

--- Function_Pointers_in_C _ Cpp.c
+++ Function_Pointers_in_C _ Cpp.c
@@ -4,13 +4,22 @@ int Add(int a, int b)
     return a + b;
 }
 
+void PrintHello()
+{
+    printf("Hello\n");
+}
+
 int main()
 {
-    int c;
-    //pointer to function that should take
-    //(int,int) as argument/parameter and return int
-    int (*p)(int, int); //(*p) parenthesis must to store a function pointer
-    p = &Add;           //Address of Add function stores in p
-    c = (*p)(2, 3);     //de-referencing and executing the function
-    printf("%d\n", c);
+    // int c;
+    // //pointer to function that should take
+    // //(int,int) as argument/parameter and return int
+    // int (*p)(int, int); //(*p) parenthesis must to store a function pointer
+    // p = &Add;           //Address of Add function stores in p
+    // c = (*p)(2, 3);     //de-referencing and executing the function
+    // printf("%d\n", c);
+
+    void (*ptr)();
+    ptr = PrintHello;
+    ptr();
 }
