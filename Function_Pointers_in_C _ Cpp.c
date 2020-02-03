ed6ecf2568b3d9cc4989d67d0c7468661df1cdfc
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-02-03 06:33:16 +0600

    Update Function_Pointers_in_C _ Cpp.c

--- Function_Pointers_in_C _ Cpp.c
+++ Function_Pointers_in_C _ Cpp.c
@@ -9,8 +9,8 @@ int main()
     int c;
     //pointer to function that should take
     //(int,int) as argument/parameter and return int
-    int (*p)(int, int);
-    p = &Add;       //Address of Add function stores in p
-    c = (*p)(2, 3); //de-referencing and executing the function
+    int (*p)(int, int); //(*p) parenthesis must to store a function pointer
+    p = &Add;           //Address of Add function stores in p
+    c = (*p)(2, 3);     //de-referencing and executing the function
     printf("%d\n", c);
 }
