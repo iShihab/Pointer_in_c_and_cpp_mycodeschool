081d9b844abf165fcf4af7d68905b38bb56d8659
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-02-03 06:05:16 +0600

    Function that returns pointer

--- Pointers_as_function_returns_in_C_cpp.c
+++ Pointers_as_function_returns_in_C_cpp.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-int Add(int a, int b)
+int *Add(int *a, int *b)
 {
-    int c = a + b;
-    return c;
+    int c = (*a) + (*b);
+    return &c;
 }
 
 int main()
